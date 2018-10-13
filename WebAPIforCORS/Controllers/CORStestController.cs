386242619617ea56cbf99b8758a6dce74fa71bc2
Max Chou <max9159@gmail.com>
2018-10-13 10:49:17 +0800

    add CORS setting

--- WebAPIforCORS/Controllers/CORStestController.cs
+++ WebAPIforCORS/Controllers/CORStestController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebAPIforCORS.Controllers
 {
+    [EnableCors(origins: "http://localhost:36054", headers: "*", methods: "*")]
     public class CORStestController : ApiController
     {
         public HttpResponseMessage Get()
