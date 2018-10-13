ea2d44bd4be0b0cd7f2f9a9d2cdb9a98a29ec8ea
Max Chou <max9159@gmail.com>
2018-10-13 10:45:22 +0800

    initialize

---
+++ WebAPIforCORS/Controllers/CORStestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIforCORS.Controllers
+{
+    public class CORStestController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Get from test.")
+            };
+        }
+
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Post from test.")
+            };
+        }
+
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Put from test.")
+            };
+        }
+
+        public HttpResponseMessage Delete()
+        {
+            return new HttpResponseMessage()
+            {
+                Content = new StringContent("Delete from test.")
+            };
+        }
+    }
+}
